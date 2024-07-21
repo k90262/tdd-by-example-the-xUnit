470bae7372904212b6d475d8f82197859ab79e47
Bill Ho <ycho.bill.lab@gmail.com>
2024-07-21 22:35:48 +0800

    program can run and print 'None'

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+class WasRun:
+    def __init__(self, name):
+        self.wasRun = None
+
 if __name__ == '__main__':
     test = WasRun("testMethod")
-    print test.wasRun
-    test.testMethod()
-    print test.wasRun
+    print(test.wasRun)
