5f8373198b721313e4fb18075d312f43fd180ffe
Bill Ho <ycho.bill.lab@gmail.com>
2024-07-24 00:12:40 +0800

    refactor: using 'Pluggable Selector' pattern

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
 
     def testMethod(self):
         self.wasRun = 1
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
 
 
 if __name__ == '__main__':
     test = WasRun("testMethod")
     print(test.wasRun)
-    test.testMethod()
+    test.run()
     print(test.wasRun)
