ffbb86380798d108b3b32409da7c984befa07669
Bill Ho <ycho.bill.lab@gmail.com>
2024-07-23 23:56:40 +0800

    green: got the result of None and 1 from attribute wasRun

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ class WasRun:
     def __init__(self, name):
         self.wasRun = None
 
+    def testMethod(self):
+        self.wasRun = 1
+
+
 if __name__ == '__main__':
     test = WasRun("testMethod")
+    print(test.wasRun)
+    test.testMethod()
     print(test.wasRun)
