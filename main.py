8328b2b9d5efd6ca2f4e60e6a07ee2a15537d026
Bill Ho <ycho.bill.lab@gmail.com>
2024-07-24 00:25:24 +0800

    refactor: create a new class TestCase

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-class WasRun:
+class TestClass:
     def __init__(self, name):
-        self.wasRun = None
         self.name = name
-
-    def testMethod(self):
-        self.wasRun = 1
-
     def run(self):
         method = getattr(self, self.name)
         method()
+
+
+class WasRun(TestClass):
+    def __init__(self, name):
+        self.wasRun = None
+        TestClass.__init__(self, name)
+
+    def testMethod(self):
+        self.wasRun = 1
 
 
 if __name__ == '__main__':
