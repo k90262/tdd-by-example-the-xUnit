ceafa559460b3a2278b6bf5ae9a0ea850207cacb
Bill Ho <ycho.bill.lab@gmail.com>
2024-07-26 00:44:17 +0800

    refactor: call setUp()

--- main.py
+++ main.py
@@ -12,27 +12,27 @@ class TestCase:
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun = 1
 
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert (not test.wasRun)
-        test.run()
-        assert (test.wasRun)
+        self.test.run()
+        assert (self.test.wasRun)
 
     def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert (test.wasSetUp)
+        self.test.run()
+        assert (self.test.wasSetUp)
 
 
 if __name__ == '__main__':
