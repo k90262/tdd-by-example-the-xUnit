f852d6f30a11c6b12add7446e853184dcc2c7cf0
Bill Ho <ycho.bill.lab@gmail.com>
2024-08-03 23:14:36 +0800

    ch21. refactor: record and show run results

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ class TestCase:
     def __init__(self, name):
         self.name = name
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
-        return TestResult()
+        return result
 
     def setUp(self):
         pass
@@ -15,8 +17,14 @@ class TestCase:
         pass
 
 class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount += 1
+
     def summary(self):
-        return "1 run, 0 failed"
+        return "%d run, 0 failed" % self.runCount
 
 
 class WasRun(TestCase):
